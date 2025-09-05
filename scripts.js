91166553dd2aef9d85873c5ec16b01f4504f15c5
kalebtavares <kalebtavares@icloud.com>
2025-09-05 10:49:05 -0400

    Adicionar espaço em branco para melhorar a legibilidade do código

--- scripts.js
+++ scripts.js
@@ -1,29 +1,27 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
 
-
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-value").value;
-  const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
+  const currencyValueToConvert = document.querySelector(
+    ".currency-value-to-convert"
+  );
   const currencyValueConverted = document.querySelector(".currency-value");
 
   console.log(currencySelect.value);
+
   const dolarToday = 5.0;
   const euroToday = 6.2;
   const convertedValue = inputCurrencyValue / dolarToday;
 
-   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
-  }).format(inputCurrencyValue)
-
+  }).format(inputCurrencyValue);
 
   currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-  }).format(convertedValue)
-
-
-  
+  }).format(convertedValue);
 }
 convertButton.addEventListener("click", convertValues);
