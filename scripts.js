90a419380d3bac3ff0b184f1f0d1ea3b57860006
kalebtavares <kalebtavares@icloud.com>
2025-09-05 08:52:29 -0400

    Adicionar classes e formatação para valores de moeda no conversor

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
-const convertButton = document.querySelector(".convert-button")
+const convertButton = document.querySelector(".convert-button");
 function convertValues() {
+  const inputCurrencyValue = document.querySelector(".input-value").value;
+  const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
+  const currencyValueConverted = document.querySelector(".currency-value");
 
-    console.log("Clicou" )
+  const dolarToday = 2.0;
+  const convertedValue = inputCurrencyValue / dolarToday;
+
+   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputCurrencyValue)
+
+
+  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(convertedValue)
+
+
+  
 }
-convertButton.addEventListener("click", convertValues)
+convertButton.addEventListener("click", convertValues);
