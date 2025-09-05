353325b2482ba89751c5e235fd35d07a6e7a7e99
kalebtavares <kalebtavares@icloud.com>
2025-09-05 10:45:44 -0400

    Adicionar classe ao select de conversão e ajustar valores de câmbio

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
+
+
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-value").value;
   const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
   const currencyValueConverted = document.querySelector(".currency-value");
 
-  const dolarToday = 2.0;
+  console.log(currencySelect.value);
+  const dolarToday = 5.0;
+  const euroToday = 6.2;
   const convertedValue = inputCurrencyValue / dolarToday;
 
    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
